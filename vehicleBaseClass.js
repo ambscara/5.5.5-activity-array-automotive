26320434b7679582f4565346f45911672d4e1df0
ambscara <150319615+ambscara@users.noreply.github.com>
2024-02-02 17:15:02 -0800

    corrected super

--- vehicleBaseClass.js
+++ vehicleBaseClass.js
@@ -1,91 +1,56 @@
-class Vehicle {
+const VehicleModule = require('./vehicle').Vehicle
+
+class Car extends VehicleModule {
     constructor(make, model, year, color, mileage) {
-        this.make = make;
-        this.model = model;
-        this.year = year;
-        this.color = color;
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 5;
         this.passenger = 0;
-        this.speed = 0;
-        this.mileage = mileage;
-        this.started = false;
-        this.numberOfWheels = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+
+    checkService() {
+        if (this.mileage > 30000) {            
+            this.scheduleService = true
+            return this.scheduleService;                       
+        }
     }
 
     start() {
-        if (this.fuel > 0) {
-            return this.started = true;
-            console.log("engine started...!!!");
+        if (this.fuel > 0) {            
+            console.log("engine has started.");
+            return this.started = true
         } else {
+            console.log("no fuel");
             return this.started = false;
-            console.log("engine cannot start...");
         }
     }
-    accelerate() {
-        if (this.started) {
-            if (this.fuel > 0) {
-                console.log(this.speed += 1);
-                this.fuel = this.fuel - 1;
+
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;               
             } else {
-                console.log("out of fuel.");
-                this.stop();
+                console.log(this.model + " " + this.make + " not have enough space to take all passengers.");
+
             }
         } else {
-            alert("You need to start the engine first.");
+            console.log(this.model + " " + this.make + " is full");
         }
     }
-    decelerate() {
-        if (this.started) {
-            if (this.fuel > 0) {
-                if (this.speed > 0) {
-                    console.log(this.speed -= 1);
-                    this.fuel = this.fuel - 1;
-                } else {
-                    console.log(this + " has stopped moving");
-                    this.fuel = this.fuel - 1;
-                }
-            } else {
-                console.log("out of fuel.");
-                this.stop();
-            }
-        } else {
-            alert("You need to start the engine first.");
-        }
-    }
-    stop() {
-        this.started = false;
-    }
 
-    drive() {
-        accelerate();
-    }
-    brake() {
-        decelerate();
-    }
 
-    autoPark()
-    {
-
-    }
-
-    autoDrive()
-    {
-      
-    }
-
-    typeOfVehicle(wheels) {
-        if (this.numberOfWheels == 8 && 8 == wheels) {
-            console.log(this.model + " " + this.make + " is a Truck");
-        } else if (this.numberOfWheels == 4 && 4 == wheels) {
-            console.log(this.model + " " + this.make + " is a CAr");
-        } else if (this.numberOfWheels == 2 && 2 == wheels) {
-            console.log(this.model + " " + this.make + " is a Bike");
-        } else {
-            console.log("Unknown type of vehicle");
-        }
-    }
 }
 
-//This exports things you want to use from this "module", more info in readme
-module.exports = {
-    Vehicle
-}
+
+let myCar = new Car('mercury', 'rad_sedan', '2002', 'white', 50000)
+
+myCar.start()
+myCar.loadPassenger(5)
+myCar.stop()
+myCar.checkService()
+
+console.log(myCar)
